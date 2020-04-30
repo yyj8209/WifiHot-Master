505b2fb34c9b6fb8c48321c3c1908630da78fc86
yyj8209 <yyj8209@163.com>
2020-04-30 11:04:03 +0800

    待调试多台机器接入的情况

--- MyServer/app/src/main/java/com/example/myserver/MainActivity.java
+++ MyServer/app/src/main/java/com/example/myserver/MainActivity.java
@@ -37,17 +37,17 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
     private static final String TAG_D = "TAG_DEBUG";
-    ServerSocket serverSocket;//创建ServerSocket对象
-    Socket clicksSocket;//连接通道，创建Socket对象
-    ExecutorService executorService;   // 创建线程池
+//    ServerSocket serverSocket;//创建ServerSocket对象
+//    Socket clicksSocket;//连接通道，创建Socket对象
+//    ExecutorService executorService;   // 创建线程池
     Receive_Thread receive_Thread;
     Button startButton;//发送按钮
     EditText portEditText, ipEditText;//端口号和IP
     EditText receiveEditText;//接收消息框
     Button sendButton;//发送按钮
     EditText sendEditText;//发送消息框
-    InputStream inputstream;//创建输入数据流
-    OutputStream outputStream;//创建输出数据流
+//    InputStream inputstream;//创建输入数据流
+//    OutputStream outputStream;//创建输出数据流
 
     ArrayList<SocketBean> ClientList;   // Set of clients(IP)
 //    public static final String Client[] = {"No.1:", "No.2:", "No.3:", "No.4:", "No.5:", "No.6:", "No.7:", "No.8:"};
@@ -72,101 +72,69 @@ public class MainActivity extends Activity {
         sendButton = (Button) findViewById(R.id.send_button);
         sendEditText = (EditText) findViewById(R.id.message_EditText);
 
-        startButton.setOnClickListener(startButtonListener);
-        sendButton.setOnClickListener(sendButtonListener);
+        startButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startServer();
+                    }
+                }).start();
+            }
+        });
+        sendButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMsg();
+            }
+        });
 
         textView[0] = findViewById(R.id.receive_TextView1);
         textView[1] = findViewById(R.id.receive_TextView2);
         textView[2] = findViewById(R.id.receive_TextView3);
         textView[3] = findViewById(R.id.receive_TextView4);
 
-        executorService = Executors.newCachedThreadPool();
+//        executorService = Executors.newCachedThreadPool();
         ClientList = new ArrayList<SocketBean>();
     }
     /**
      * 启动服务按钮监听事件
      */
-    private OnClickListener startButtonListener = new OnClickListener() {
+    private void startServer() {
 
-        @Override
-        public void onClick(View v) {
-            // TODO Auto-generated method stub
-            /**
-             * 启动服务器监听线程
-             */
-//            ClientCode = 0;
-            ServerSocket_thread serverSocket_thread = new ServerSocket_thread();
-            serverSocket_thread.start();
-        }
-    };
-
-    /**
-     * 服务器监听线程
-     */
-    class ServerSocket_thread extends Thread
-    {
-        @Override
-        public void run()//重写Thread的run方法
-        {
-            try
-            {
-                int port =Integer.valueOf(portEditText.getText().toString());//获取portEditText中的端口号
+        ServerSocket serverSocket = null;
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        int port =Integer.valueOf(portEditText.getText().toString());//获取portEditText中的端口号
+        try{
                 serverSocket = new ServerSocket(port);//监听port端口，这个程序的通信端口就是port了
                 ipEditText.setText(getHostIpAddress());
-            }
-            catch (IOException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            try
-            {
                 while (isStart)
                 {
                     //监听连接 ，如果无连接就会处于阻塞状态，一直在这等着
-                    clicksSocket = serverSocket.accept();
+                    Socket clicksSocket = serverSocket.accept();
                     clicksSocket.setSoTimeout(5000);
-//                    inputstream = clicksSocket.getInputStream();//
-                    // 为了显示多个客户端
                     CurrentClient = getClientIpAddress(clicksSocket).replace("/","");
-                    //启动接收线程
-                    receive_Thread = new Receive_Thread(clicksSocket);
-                    receive_Thread.start();
                     final SocketBean socketBean = new SocketBean("",clicksSocket);
                     socketBean.id = CurrentClient;
                     socketBean.socket = clicksSocket;
+                    receive_Thread = new Receive_Thread(clicksSocket);
 
                     if (clicksSocket.isConnected()) {
                         ClientList.add(socketBean);    // Insert client to clientset.
+                        //启动接收线程
+//                        receive_Thread.start();
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 receiveEditText.setText("当前接入："+CurrentClient);
                             }
                         });
-                        Log.e(TAG_D,"客户端数量："+ClientList.size());
-//                        executorService.execute(receive_Thread);
-
-//                    new Thread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            try {
-////                                inputstream = clicksSocket.getInputStream();//
-//                                outputStream = clicksSocket.getOutputStream();
-//                                if (outputStream != null) {
-//                                    outputStream.write(CurrentClient.getBytes("utf-8"));
-//                                }
-////                                    Thread.sleep(10);
-////                                    outputStream.close();
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    }).start();
-
+                        Log.d(TAG_D,"客户端数量："+ClientList.size());
+                        executorService.execute(receive_Thread);
                     }
                 }
-//                clicksSocket.close();     // 关闭所有线程，可以用清理线程池的方式。
+                serverSocket.close();     // 关闭服务器，所有线程，可以用清理线程池的方式。
 //                executorService.shutdownNow();
             }
             catch (IOException e)
@@ -177,17 +145,107 @@ public class MainActivity extends Activity {
                 if (serverSocket != null) {
                     try {
 //                        isStart = false;
-                        receive_Thread.interrupt();
-                        Log.e(TAG_D,"接收线程关闭");
+                        Log.d(TAG_D,"接收线程关闭");
 //                        executorService.shutdown();
 //                        inputstream.close();
 //                        outputStream.close();
                         serverSocket.close();
+                        receive_Thread.interrupt();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }
+    };
+
+    /**
+     * 服务器监听线程
+     */
+    class ServerSocket_thread extends Thread
+    {
+        @Override
+        public void run()//重写Thread的run方法
+        {
+//            try
+//            {
+//                int port =Integer.valueOf(portEditText.getText().toString());//获取portEditText中的端口号
+//                serverSocket = new ServerSocket(port);//监听port端口，这个程序的通信端口就是port了
+//                ipEditText.setText(getHostIpAddress());
+//            }
+//            catch (IOException e)
+//            {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//            try
+//            {
+//                while (isStart)
+//                {
+//                    //监听连接 ，如果无连接就会处于阻塞状态，一直在这等着
+//                    clicksSocket = serverSocket.accept();
+//                    clicksSocket.setSoTimeout(5000);
+////                    inputstream = clicksSocket.getInputStream();//
+//                    // 为了显示多个客户端
+//                    CurrentClient = getClientIpAddress(clicksSocket).replace("/","");
+//                    //启动接收线程
+//                    receive_Thread = new Receive_Thread(clicksSocket);
+//                    receive_Thread.start();
+//                    final SocketBean socketBean = new SocketBean("",clicksSocket);
+//                    socketBean.id = CurrentClient;
+//                    socketBean.socket = clicksSocket;
+//
+//                    if (clicksSocket.isConnected()) {
+//                        ClientList.add(socketBean);    // Insert client to clientset.
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                receiveEditText.setText("当前接入："+CurrentClient);
+//                            }
+//                        });
+//                        Log.d(TAG_D,"客户端数量："+ClientList.size());
+////                        executorService.execute(receive_Thread);
+//
+////                    new Thread(new Runnable() {
+////                        @Override
+////                        public void run() {
+////                            try {
+//////                                inputstream = clicksSocket.getInputStream();//
+////                                outputStream = clicksSocket.getOutputStream();
+////                                if (outputStream != null) {
+////                                    outputStream.write(CurrentClient.getBytes("utf-8"));
+////                                }
+//////                                    Thread.sleep(10);
+//////                                    outputStream.close();
+////                            } catch (IOException e) {
+////                                e.printStackTrace();
+////                            }
+////                        }
+////                    }).start();
+//
+//                    }
+//                }
+////                clicksSocket.close();     // 关闭所有线程，可以用清理线程池的方式。
+////                executorService.shutdownNow();
+//            }
+//            catch (IOException e)
+//            {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } finally {
+//                if (serverSocket != null) {
+//                    try {
+////                        isStart = false;
+//                        receive_Thread.interrupt();
+//                        Log.d(TAG_D,"接收线程关闭");
+////                        executorService.shutdown();
+////                        inputstream.close();
+////                        outputStream.close();
+//                        serverSocket.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
         }
     }
     /**
@@ -201,7 +259,7 @@ public class MainActivity extends Activity {
         Receive_Thread(){};
         Receive_Thread(Socket socket){
             this.socket = socket;
-            Log.e(TAG_D,"启动新线程");
+            Log.d(TAG_D,"启动新线程");
         }
 
         @Override
@@ -210,12 +268,12 @@ public class MainActivity extends Activity {
             try
             {
                 while(true){
-//                    Log.e(TAG_D,String.valueOf(socket.isClosed()));
+//                    Log.d(TAG_D,String.valueOf(socket.isClosed()));
                     final byte[] buf = new byte[1024];
                     final InputStream is = socket.getInputStream();
                     final OutputStream os = socket.getOutputStream();   // 需要和线程对应起来
                     final int len = is.read(buf);
-                    Log.e(TAG_D,new String(buf,0,len));
+                    Log.d(TAG_D,new String(buf,0,len));
                     runOnUiThread(new Runnable()
                     {
                         public void run()
@@ -252,10 +310,7 @@ public class MainActivity extends Activity {
     /**
      * 发送消息按钮事件
      */
-    private OnClickListener sendButtonListener = new OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
+    private void sendMsg() {
         Toast.makeText(MainActivity.this, "reserved", Toast.LENGTH_LONG).show();
             // TODO Auto-generated method stub
 //            new Thread(new Runnable() {
@@ -272,8 +327,6 @@ public class MainActivity extends Activity {
 //                    }
 //                }
 //            }).start();
-
-        }
     };
     /**
      *
@@ -319,16 +372,16 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
 
-        if (serverSocket != null) {
-            try {
-                Log.e(TAG_D,"ServerSocket closed.");
-//                        executorService.shutdown();
-//                        inputstream.close();
-//                        outputStream.close();
-                serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+//        if (serverSocket != null) {
+//            try {
+//                Log.d(TAG_D,"ServerSocket closed.");
+////                        executorService.shutdown();
+////                        inputstream.close();
+////                        outputStream.close();
+//                serverSocket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
